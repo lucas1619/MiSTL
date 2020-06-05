f006e5f216fe50a4871d8bda11d167833cdfde89
Lucas <lucas.moreno.olivos@gmail.com>
2020-06-04 19:53:29 -0500

    lista doble enlazada

--- MiSTL/Lista.hpp
+++ MiSTL/Lista.hpp
@@ -202,53 +202,171 @@ namespace listaSimple{
     };
 }
 namespace listaDoble{
-    using namespace enlaceDoble;
-    template <class K>
-    class Lista{
-    private:
-        Nodo<K>* inicio;
-        Nodo<K>* fin;
-        long long tamanho;
-        Nodo<K>* crearNodo(K elemento){
-            Nodo<K> * nuevo = new Nodo<K>();
-            nuevo->elemento = elemento;
-            return nuevo;
-        }
-    public:
-        Lista(){
-            this->inicio = this->fin = nullptr;
-            this->tamanho = 0;
-        }
-        ~Lista(){
-        }
-        void insertarEnPosicion(long long posicion, K elemento){
-            Nodo<K>* nuevo = this->crearNodo(elemento);
-            Nodo<K>* puntero = this->inicio;
-            long long contador = 0;
-            while (contador != posicion - 1){
-                puntero = puntero->siguiente;
-                ++contador;
-            }
-            puntero->siguiente->anterior = nuevo;
-            nuevo->siguiente = puntero->siguiente;
-            nuevo->anterior = puntero;
-            puntero->siguiente = nuevo;
-            ++this->tamanho;
-        }
-        void eliminarEnPosicion(long long posicion){
-            Nodo<K>*puntero = this->inicio;
-            long long contador = 0;
-            while (contador != posicion - 1){
-                puntero = puntero->siguiente;
-                ++contador;
-            }
-            Nodo<K>*eliminado = puntero->siguiente;
-            puntero->siguiente = eliminado->siguiente;
-            eliminado->siguiente->anterior = puntero;
-            eliminado->anterior = eliminado->siguiente = nullptr;
-            delete eliminado;
-            --this->tamanho;
-        }
-    };
+	using namespace enlaceDoble;
+	template <class K>
+	class Lista {
+	private:
+		Nodo<K> *inicio;
+		Nodo<K> *fin;
+		long long tamanho;
+		Nodo<K> *crearNodo(K elemento) {
+			Nodo<K> *nuevo = new Nodo<K>();
+			nuevo->elemento = elemento;
+			return nuevo;
+		}
+	public:
+		Lista() {
+			this->inicio = this->fin = nullptr;
+			this->tamanho = 0;
+		}
+		~Lista() {
+			while (!this->estaVacia()) {
+				this->eliminarAlInicio();
+			}
+			delete this->inicio;
+			delete this->fin;
+		}
+
+		void insertarAlInicio(K elemento) {
+			Nodo<K> *nuevo = crearNodo(elemento);
+			if (this->tamanho > 0) {
+				nuevo->siguiente = this->inicio;
+				this->inicio->anterior = nuevo;
+				this->inicio = nuevo;
+			}
+			else {
+				this->inicio = this->fin = nuevo;
+			}
+			++this->tamanho;
+		}
+
+		void insertarAlFinal(K elemento) {
+			if (this->tamanho > 0) {
+				Nodo<K> *nuevo = crearNodo(elemento);
+				this->fin->siguiente = nuevo;
+				nuevo->anterior = this->fin;
+				this->fin = nuevo;
+				++this->tamanho;
+			}
+			else {
+				this->insertarAlInicio(elemento);
+			}
+		}
+
+		void insertarEnPos(long long pos, K elemento) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					this->insertarAlInicio(elemento);
+				}
+				else if (pos == this->tamanho - 1) {
+					this->insertarAlFinal(elemento);
+				}
+				else {
+					Nodo<K> *nuevo = this->crearNodo(elemento);
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos - 1) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					puntero->siguiente->anterior = nuevo;
+					nuevo->siguiente = puntero->siguiente;
+					nuevo->anterior = puntero;
+					puntero->siguiente = nuevo;
+					++this->tamanho;
+				}
+			}
+		}
+
+		void eliminarAlInicio() {
+			if (this->tamanho > 0) {
+				if (this->tamanho == 1) {
+					delete this->inicio;
+					this->inicio = this->fin = nullptr;
+				}
+				else {
+					Nodo<K> *eliminado = this->inicio;
+					this->inicio = eliminado->siguiente;
+					eliminado->siguiente->anterior = nullptr;
+					eliminado->siguiente = nullptr;
+					delete eliminado;
+				}
+				--this->tamanho;
+			}
+		}
+
+		void eliminarAlFinal() {
+			if (this->tamanho <= 1) {
+				this->eliminarAlInicio();
+			}
+			else {
+				Nodo<K> *eliminado = this->fin;
+				eliminado->anterior->siguiente = nullptr;
+				this->fin = eliminado->anterior;
+				eliminado->anterior = nullptr;
+				delete eliminado;
+				--this->tamanho;
+			}
+		}
+
+		void eliminarEnPos(long long pos) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					this->eliminarAlInicio();
+				}
+				else if (pos == this->tamanho - 1) {
+					this->eliminarAlFinal();
+				}
+				else {
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos - 1) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					Nodo<K> *eliminado = puntero->siguiente;
+					puntero->siguiente = eliminado->siguiente;
+					eliminado->siguiente->anterior = puntero;
+					eliminado->anterior = eliminado->siguiente = nullptr;
+					delete eliminado;
+					--this->tamanho;
+				}
+			}
+		}
+
+		K obtenerElementoAlInicio() {
+			return this->inicio->elemento;
+		}
+
+		K obtenerElementoAlFinal() {
+			return this->fin->elemento;
+		}
+
+		K obtenerElementoEnPos(long long pos) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					return this->obtenerElementoAlInicio();
+				}
+				else if (pos == this->tamanho - 1) {
+					return this->obtenerElementoAlFinal();
+				}
+				else {
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					return puntero->elemento;
+				}
+			}
+		}
+		long long obtenerTamanho() {
+			return this->tamanho;
+		}
+		bool estaVacia() {
+			return this->tamanho == 0;
+		}
+	};
 }
 #endif //MISTL_LISTA_HPP
--- MiSTL/ListaCircular.hpp
+++ MiSTL/ListaCircular.hpp
@@ -165,10 +165,191 @@ namespace listaCircularSimple{
     };
 }
 namespace listaCircularDoble{
-    using namespace enlaceDoble;
-    template <class K>
-    class ListaCircular{
+	using namespace enlaceDoble;
+	template <class K>
+	class ListaCircular {
+	private:
+		Nodo<K> *inicio;
+		Nodo<K> *fin;
+		long long tamanho;
+		//es para que a cada rato no estés creandolo, mejor solo llamas a esta función
+		Nodo<K>* crearNodo(K elemento) {
+			Nodo<K> *nuevo = new Nodo<K>();
+			nuevo->elemento = elemento;
+			return nuevo;
+		}
+	public:
+		ListaCircular() {
+			this->inicio = this->fin = nullptr;
+			tamanho = 0;
+		}
+		~ListaCircular() {
 
-    };
+		}
+
+		void insertarAlInicio(K elemento) {
+			Nodo<K> *nuevo = crearNodo();
+			if (this->tamanho == 0) {
+				this->fin = this->inicio = nuevo;
+				nuevo->siguiente = nuevo;
+				nuevo->anterior = nuevo;
+			}
+			else {
+				nuevo->siguiente = this->inicio;
+				this->fin->siguiente = nuevo;
+				this->inicio->anterior = nuevo;
+				nuevo->anterior = this->fin;
+				this->inicio = nuevo;
+			}
+			++this->tamanho;
+		}
+
+		void insertarAlFinal(K elemento) {
+			if (this->tamanho == 0) {
+				this->insertarAlInicio(elemento);
+			}
+			else {
+				Nodo<K> *nuevo = crearNodo(elemento);
+				this->fin->siguiente = nuevo;
+				nuevo->anterior = this->fin;
+				nuevo->siguiente = this->inicio;
+				this->fin = nuevo;
+				this->inicio->anterior = nuevo;
+				++this->tamanho;
+			}
+		}
+
+		void insertarEnPos(long long pos, K elemento) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					this->insertarAlInicio(elemento);
+				}
+				else if (pos == this->tamanho - 1) {
+					this->insertarAlFinal(elemento);
+				}
+				else {
+					Nodo<K> *nuevo = this->crearNodo(elemento);
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos - 1) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					puntero->siguiente->anterior = nuevo;
+					nuevo->siguiente = puntero->siguiente;
+					nuevo->anterior = puntero;
+					puntero->siguiente = nuevo;
+					++this->tamanho;
+				}
+			}
+		}
+
+		void eliminarAlInicio() {
+			if (this->tamanho > 0) {
+				if (this->tamanho == 1) {
+					this->inicio->siguiente = nullptr;
+					this->inicio->anterior = nullptr;
+					delete this->inicio;
+					this->inicio = this->fin = nullptr;
+				}
+				else {
+					Nodo<K> *eliminado = this->inicio;
+					eliminado->siguiente->anterior = nullptr;
+					this->inicio = eliminado->siguiente;
+					eliminado->siguiente = nullptr;
+					eliminado->anterior = nullptr;
+					this->fin->siguiente = this->inicio;
+					this->inicio->anterior = this->fin;
+					delete eliminado;
+				}
+				--this->tamanho;
+			}
+		}
+
+		void eliminarAlFinal() {
+			if (this->tamanho <= 1) {
+				this->eliminarAlInicio();
+			}
+			else {
+				Nodo<K> *eliminado = this->fin;
+				this->inicio->anterior = eliminado->anterior;
+				eliminado->anterior->siguiente = this->inicio;
+				this->fin = eliminado->anterior;
+				eliminado->siguiente = nullptr;
+				eliminado->anterior = nullptr;
+				delete eliminado;
+				--this->tamanho;
+			}
+		}
+
+		void eliminarEnPos(long long pos) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					this->eliminarAlInicio();
+				}
+				else if (pos == this->tamanho - 1) {
+					this->eliminarAlFinal();
+				}
+				else {
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos - 1) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					Nodo<K> *eliminado = puntero->siguiente;
+					puntero->siguiente = eliminado->siguiente;
+					eliminado->siguiente->anterior = puntero;
+					eliminado->anterior = eliminado->siguiente = nullptr;
+					delete eliminado;
+					--this->tamanho;
+				}
+			}
+		}
+
+		K obtenerElementoAlInicio() {
+			return this->inicio->elemento;
+		}
+
+		K obtenerElementoAlFinal() {
+			return this->fin->elemento;
+		}
+
+		K obtenerElementoEnPos(long long pos) {
+			if (pos < this->tamanho && pos >= 0) {
+				if (pos == 0) {
+					return this->obtenerElementoAlInicio();
+				}
+				else if (pos == this->tamanho - 1) {
+					return this->obtenerElementoAlFinal();
+				}
+				else {
+					Nodo<K> *puntero = this->inicio;
+					long long cont = 0;
+					while (cont != pos) {
+						puntero = puntero->siguiente;
+						++cont;
+					}
+					return puntero->elemento;
+				}
+			}
+		}
+		long long obtenerTamanho() {
+			return this->tamanho;
+		}
+		bool estaVacia() {
+			return this->tamanho == 0;
+		}
+
+		K obtenerIterandoNVeces(long long n) {
+			Nodo<K> *puntero = this->inicio;
+			long long cont = 0;
+			while (cont != n) {
+				puntero = puntero->siguiente;
+				++cont;
+			}
+			return puntero->elemento;
+		}
+	};
 }
 #endif //MISTL_LISTACIRCULAR_HPP
